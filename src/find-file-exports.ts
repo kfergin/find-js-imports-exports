cf6d56410cd9e8f67bcd25e97429ab4308260866
Kaleb Fergin <kaleb@kalebfergin.com>
2022-07-17 16:44:40 -0600

    Find re-exported exports from another file
    Also, updates tsconfig.json because it was complaining about a
    `for...of` loop. TS recommended a base config, so I updated based on
    that: https://github.com/tsconfig/bases/blob/main/bases/node17.json

--- src/find-file-exports.ts
+++ src/find-file-exports.ts
@@ -3,7 +3,10 @@
 // it with a config (?)
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
+import { dirname } from 'path';
+
 import { traverse } from 'estraverse';
+import resolveFrom from 'resolve-from';
 
 import { getAstFromPath } from './get-ast-from-path';
 
@@ -15,6 +18,10 @@ export async function findFileExports(filePath: string) {
     name: string;
     source: string;
   }[] = [];
+
+  // if this file re-exports from other files, we'll include those too
+  // e.g. export * from 'somewhere';
+  const exportStarPaths: Set<string> = new Set();
 
   traverse(ast, {
     enter: function (node) {
@@ -78,10 +85,36 @@ export async function findFileExports(filePath: string) {
               });
           }
         }
+      } else if (node.type === 'ExportAllDeclaration') {
+        if (node.exported) {
+          // export * as something from 'somewhere';
+          fileExports.push({
+            lineNumber: node.exported.loc!.start.line,
+            name: node.exported.name,
+            source: filePath,
+          });
+        } else {
+          // export * from 'somewhere';
+          const exportStarPath = resolveFrom.silent(
+            dirname(filePath),
+            node.source.value as string
+          );
+          if (exportStarPath) {
+            exportStarPaths.add(exportStarPath);
+          }
+        }
       }
     },
     fallback: 'iteration',
   });
+
+  for (const exportStarPath of exportStarPaths) {
+    // NOTE: this will include default exports, but in the current spec,
+    // `export *` doesn't actually include that export. I'm keeping this as is
+    // because you shouldn't rely on this behavior, which may change in a
+    // future spec.
+    fileExports.push(...(await findFileExports(exportStarPath)));
+  }
 
   return fileExports;
 }
